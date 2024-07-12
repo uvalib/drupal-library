68c805382e0386d827f534eb4aebe235843196d9
ys2n <ys2n@virginia.edu>
2024-07-12 13:30:11 -0400

    made fetch-backup.sh a little smarter

--- local/ddev/backups/fetch-backup.sh
+++ local/ddev/backups/fetch-backup.sh
@@ -1,3 +1,12 @@
-#!/bin/bash -x
+#!/bin/bash
 
-ssh -t library-drupal-dev-0.internal.lib.virginia.edu sudo docker exec -it drupal-0 drush sql-dump --extra-dump=--no-tablespaces | gzip > ./library-drupal-dev-backup.`date +%F-%T`.sql.gz
+OUTPUT=./sql/library-drupal-dev-backup.`date +%F-%T`.sql.gz
+SRCHOST=library-drupal-dev-0.internal.lib.virginia.edu 
+echo "NOTE: this script assumes you have ssh, sudo and docker privileges on $SRCHOST"
+echo "Dumping from $SRCHOST to $OUTPUT"
+
+ssh -t $SRCHOST sudo docker exec -it drupal-0 drush sql-dump --extra-dump=--no-tablespaces | gzip > $OUTPUT
+
+( gzip -t $OUTPUT 2>/dev/null && [ "$(gunzip -c $OUTPUT | wc -c)" -ne 0 ]) \
+	&& echo "Successfully wrote $OUTPUT" \
+	|| echo "ERROR: The gzip file is truncated or corrupted."; mv ${OUTPUT} ${OUTPUT}.corrupted
