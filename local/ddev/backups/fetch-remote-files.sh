064a12d6e67880c6869bf70c556660e9dc74bece
ys2n <ys2n@virginia.edu>
2024-08-17 01:05:15 -0400

    changed remote host to production in fetch-backup.sh ahd fetch-remote-files.sh

--- local/ddev/backups/fetch-remote-files.sh
+++ local/ddev/backups/fetch-remote-files.sh
@@ -2,9 +2,9 @@
 
 LOCAL_DIR="../web/sites/default/files"
 
-REMOTE_HOST="library-drupal-develop-0.internal.lib.virginia.edu"
+REMOTE_HOST="library-drupal-0.internal.lib.virginia.edu"
 # SSH options
 
-rsync -avz -e ssh library-drupal-develop-0.internal.lib.virginia.edu:/mnt/data/drupal-0/sites/default/files/ $LOCAL_DIR
+rsync -avz -e ssh ${REMOTE_HOST}:/mnt/data/drupal-0/sites/default/files/ $LOCAL_DIR
 
 echo "Rsync download completed successfully."
